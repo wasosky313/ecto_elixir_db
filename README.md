d2f963abbcd9ce6cae06876317c3023ba4072a0a
Andy Jimenez Reyes <wasosky313@gmail.com>
2022-08-29 21:01:38 -0300

    Created base project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ecto_elixir_db
+
+# Install phoenix
+mix archive.install hex phx_new
+
+# create new project without html
+mix phx.new petex --no-html --no-assets
---
+++ petex/lib/petex/application.ex
@@ -0,0 +1,36 @@
+defmodule Petex.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Start the Ecto repository
+      Petex.Repo,
+      # Start the Telemetry supervisor
+      PetexWeb.Telemetry,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: Petex.PubSub},
+      # Start the Endpoint (http/https)
+      PetexWeb.Endpoint
+      # Start a worker by calling: Petex.Worker.start_link(arg)
+      # {Petex.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Petex.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    PetexWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
